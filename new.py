ed8245128038d3884adce599ec94664c40a147b5
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-20 15:48:09 +0300

    few changes

--- new.py
+++ new.py
@@ -1,2 +1,10 @@
 from functions import *
-print(mean([1, 3, 6]))
+students = {'John':50, 'James':60}
+
+
+
+temprature = [10,20,30,40,50]
+
+print("the mean for " +str(len(students)) + " students is " + str(mean(students)  ) )
+
+print("The average temprarure is " +  str(mean(temprature)))
