c14e41997826720b81c2eddb5a0123b972e392d3
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-20 16:48:20 +0300

    loop of

--- new.py
+++ new.py
@@ -8,3 +8,7 @@ temprature = [10,20,30,40,50]
 print("the mean for " +str(len(students)) + " students is " + str(mean(students)  ) )
 
 print("The average temprarure is " +  str(mean(temprature)))
+
+for student in students.keys():
+    #you can also use students.items to get key value pair
+    print(student)
