b7dc8d0ec4f8caa981f8a811f03a46963724fa7c
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-20 23:16:46 +0300

    kwags

--- list_comprehension.py
+++ list_comprehension.py
@@ -10,3 +10,23 @@ def make_list(lists):
     new_list = [lst for lst in lists if type(lst)==int]
     return new_list
 print(make_list(lists))
+
+listss = [30, 1, 4, 45, 12]
+def list_maker(lists):
+    new_list =[lst for lst in listss if lst>5]
+    return new_list
+print(list_maker(lists))
+
+#function for args. *args are the urguiments to take in for a function
+def mean(*args):
+    the_mean = sum(args)/len(args)
+    return the_mean
+
+print(mean(2,4,6))
+
+#kwarg. here the arguments have to be assigned
+
+def find_sum(**kwargs):
+    total = sum(kwargs.values())
+    return total
+print(find_sum(a=5,b=11))
